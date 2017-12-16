24a7a6b598ce310a0a8d2954016325a0f14fa5ea
Karol Bukowski <buddy@Mac-mini-Karol.home>
2017-12-17 00:20:10 +0100

    add Double Horizontal layout

---
+++ OneWord/Layouts/DoubleHorizontalLayoutViewController.swift
@@ -0,0 +1,91 @@
+//
+//  DoubleHorizontalViewController.swift
+//  OneWord
+//
+//  Created by Karol Bukowski on 16.12.2017.
+//  Copyright © 2017 Karol Bukowski. All rights reserved.
+//
+
+import UIKit
+
+protocol DoubleHorizontalLayoutProtocol: class {
+    var topViewHeight: CGFloat { get }
+}
+
+protocol DoubleHorizontalViewModelProtocol {
+    var topViewController: UIViewController { get }
+    var bottomViewController: UIViewController { get }
+}
+
+class DoubleHorizontalLayoutViewController: UIViewController {
+    // MARK:- Properties
+    private var viewModel: DoubleHorizontalViewModelProtocol
+    weak var layout: DoubleHorizontalLayoutProtocol?
+    
+    private lazy var topView: UIView = {
+        let frame = CGRect(x: self.view.bounds.origin.x,
+                           y: self.view.bounds.origin.y,
+                           width: self.view.bounds.width,
+                           height: self.layout?.topViewHeight ?? 0)
+        
+        let view = UIView(frame: frame)
+        
+        view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
+        view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        view.topAnchor.constraint(equalTo: self.view.topAnchor)
+        
+        return view
+    }()
+    
+    private lazy var bottomView: UIView = {
+        let frame = CGRect(x: self.view.bounds.origin.x,
+                           y: self.topView.bounds.maxY,
+                           width: self.view.bounds.width,
+                           height: self.view.bounds.height - self.topView.bounds.height)
+        
+        let view = UIView(frame: frame)
+        
+        view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
+        view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        view.topAnchor.constraint(equalTo: self.topView.bottomAnchor)
+        
+        return view
+    }()
+    
+    // MARK:- Initialization
+    init(viewModel: DoubleHorizontalViewModelProtocol) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        self.init(coder: aDecoder)
+    }
+    
+    private func setupView() {
+        self.view.addSubview(topView)
+        self.view.addSubview(bottomView)
+        
+        let topViewController = viewModel.topViewController
+        topViewController.view.frame = self.topView.frame
+        self.addChildViewController(topViewController)
+        self.topView.addSubview(topViewController.view)
+        topViewController.didMove(toParentViewController: self)
+        
+        let bottomViewController = viewModel.bottomViewController
+        bottomViewController.view.frame = self.bottomView.frame
+        self.addChildViewController(bottomViewController)
+        self.topView.addSubview(bottomViewController.view)
+        bottomViewController.didMove(toParentViewController: self)
+    }
+    
+    // MARK:- Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupView()
+    }
+}
+
