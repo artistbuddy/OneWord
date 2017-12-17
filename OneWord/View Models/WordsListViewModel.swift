f107c250d1193991ffd1ca71f2bfe623451c6693
Karol Bukowski <buddy@Mac-mini-Karol.home>
2017-12-17 17:51:40 +0100

    implement WordsListViewModel

--- OneWord/View Models/WordsListViewModel.swift
+++ OneWord/View Models/WordsListViewModel.swift
@@ -6,16 +6,41 @@
 //  Copyright © 2017 Karol Bukowski. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-// TODO: implement WordsListViewModel
-class WordsListViewModel {
-    init() {
-        
-    }
+struct FakeWordsDataSource {
+    var words = ["hello", "bannana", "house", "job"]
+}
+
+class WordsListViewModel: NSObject {
+    let list = FakeWordsDataSource()
 }
 
 // MARK:- WordsListViewModelProtocol
 extension WordsListViewModel: WordsListViewModelProtocol {
+    var emptyListMessage: String {
+        return "Click Add button to add new words"
+    }
     
+    var shouldHideWordsList: Bool {
+        return self.list.words.count == 0
+    }
 }
+
+// MARK:- UITableViewDataSource
+extension WordsListViewModel: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "WordListCell")
+        
+        cell.textLabel?.text = self.list.words[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK:- UITableViewDelegate
+extension WordsListViewModel: UITableViewDelegate { }
